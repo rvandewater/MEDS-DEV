2fbdc5fed9c9206c1e06e9cd0c3a430fe9a0bd13
Robin van de Water <r.p.vandewater@gmail.com>
2025-10-22 13:38:05 +0200

    update

--- run_models_slurm.sh
+++ run_models_slurm.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 #SBATCH --job-name=meds_dev_experiments # -J
-#SBATCH --partition=cpu # -p
+#SBATCH --partition=gpu # -p
 #SBATCH --cpus-per-task=16 # -c
 #SBATCH --mem=200gb
+#SBATCH --gpus=1
 #SBATCH --output=../%x/%x_%j.log # %x is job-name, %j is job id
 #SBATCH --account=sci-lippert
 #SBATCH --time=120:00:00 # -t
+    # !/bin/bash
+    # SBATCH --job-name=meds_dev_experiments # -J
+    # SBATCH --partition=cpu # -p
+    # SBATCH --cpus-per-task=16 # -c
+    # SBATCH --mem=200gb
+    # SBATCH --output=../%x/%x_%j.log # %x is job-name, %j is job id
+    # SBATCH --account=sci-lippert
+    # SBATCH --time=120:00:00 # -t
 eval "$(conda shell.bash hook)"
 cd ~/projects/MEDS_DEV_NEW || exit
 conda activate meds_dev_311
