624856fcec8b8d79516f7cb8206fb88487b82a2b
Robin van de Water <r.p.vandewater@gmail.com>
2025-11-01 12:58:07 +0100

    path

--- scripts/run_all_models.sh
+++ scripts/run_all_models.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
-sbatch run_models_slurm.sh HIRID
-sbatch run_models_slurm.sh MIMIC-IV
-sbatch run_models_slurm.sh INSPIRE
-sbatch run_models_slurm.sh NWICU
-sbatch run_models_slurm.sh SICdb
-sbatch run_models_slurm.sh AUMCdb
-# sbatch run_models_slurm.sh eICU
-# sbatch run_models_slurm.sh EHRShot
+export current_path=result=${result:-/}
+sbatch "${current_path}"/run_models_slurm.sh HIRID
+sbatch "${current_path}"/run_models_slurm.sh MIMIC-IV
+sbatch "${current_path}"/run_models_slurm.sh INSPIRE
+sbatch "${current_path}"/run_models_slurm.sh NWICU
+sbatch "${current_path}"/run_models_slurm.sh SICdb
+sbatch "${current_path}"/run_models_slurm.sh AUMCdb
+# sbatch "${current_path}"/run_models_slurm.sh eICU
+# sbatch "${current_path}"/run_models_slurm.sh EHRShot
