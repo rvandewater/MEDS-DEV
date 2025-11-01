2c5b8740625a83a1cb92f02bc5f2e12830e9df7a
Robin van de Water <r.p.vandewater@gmail.com>
2025-11-01 18:02:35 +0100

    cehrbert

--- scripts/run_all_models.sh
+++ scripts/run_all_models.sh
@@ -7,11 +7,11 @@ if [ "$#" -lt 1 ]; then
 fi
 export MODEL_NAME=$1
 
-sbatch --job-name="${MODEL_NAME}_HIRID" "${current_path}/run_models_slurm.sh" "$MODEL_NAME" HIRID
-sbatch --job-name="${MODEL_NAME}_MIMIC-IV" "${current_path}/run_models_slurm.sh" "$MODEL_NAME" MIMIC-IV
+# sbatch --job-name="${MODEL_NAME}_HIRID" "${current_path}/run_models_slurm.sh" "$MODEL_NAME" HIRID
+# sbatch --job-name="${MODEL_NAME}_MIMIC-IV" "${current_path}/run_models_slurm.sh" "$MODEL_NAME" MIMIC-IV
 sbatch --job-name="${MODEL_NAME}_INSPIRE" "${current_path}/run_models_slurm.sh" "$MODEL_NAME" INSPIRE
 sbatch --job-name="${MODEL_NAME}_NWICU" "${current_path}/run_models_slurm.sh" "$MODEL_NAME" NWICU
-sbatch --job-name="${MODEL_NAME}_SICdb" "${current_path}/run_models_slurm.sh" "$MODEL_NAME" SICdb
+# sbatch --job-name="${MODEL_NAME}_SICdb" "${current_path}/run_models_slurm.sh" "$MODEL_NAME" SICdb
 sbatch --job-name="${MODEL_NAME}_AUMCdb" "${current_path}/run_models_slurm.sh" "$MODEL_NAME" AUMCdb
 
 # sbatch "${current_path}"/run_models_slurm.sh "$MODEL_NAME" eICU
