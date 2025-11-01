ec1ff612356d07bae3eb53db06e2621614a7a688
Robin van de Water <r.p.vandewater@gmail.com>
2025-11-01 13:00:56 +0100

    path

--- scripts/run_all_models.sh
+++ scripts/run_all_models.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-export current_path=result=${result:-/}
-sbatch "${current_path}"/run_models_slurm.sh HIRID
-sbatch "${current_path}"/run_models_slurm.sh MIMIC-IV
-sbatch "${current_path}"/run_models_slurm.sh INSPIRE
-sbatch "${current_path}"/run_models_slurm.sh NWICU
-sbatch "${current_path}"/run_models_slurm.sh SICdb
-sbatch "${current_path}"/run_models_slurm.sh AUMCdb
+
+current_path=$(pwd) # Dynamically set the current directory path
+sbatch "${current_path}/run_models_slurm.sh" HIRID
+sbatch "${current_path}/run_models_slurm.sh" MIMIC-IV
+sbatch "${current_path}/run_models_slurm.sh" INSPIRE
+sbatch "${current_path}/run_models_slurm.sh" NWICU
+sbatch "${current_path}/run_models_slurm.sh" SICdb
+sbatch "${current_path}/run_models_slurm.sh" AUMCdb
 # sbatch "${current_path}"/run_models_slurm.sh eICU
 # sbatch "${current_path}"/run_models_slurm.sh EHRShot
