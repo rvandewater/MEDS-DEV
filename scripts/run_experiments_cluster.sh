b04e582c637872da020469e3a75db867eabcbf22
Robin van de Water <r.p.vandewater@gmail.com>
2025-11-01 17:43:32 +0100

    fix command

--- scripts/run_experiments_cluster.sh
+++ scripts/run_experiments_cluster.sh
@@ -4,9 +4,18 @@
 #SBATCH --cpus-per-task=16 # -c
 #SBATCH --mem=200gb
 #SBATCH --gpus=1
+#SBATCH --gpus=a40:1
 #SBATCH --output=../%x/%x_%j.log # %x is job-name, %j is job id
 #SBATCH --account=sci-lippert
 #SBATCH --time=120:00:00 # -t
+    # !/bin/bash
+    # SBATCH --job-name=meds_dev_experiments # -J
+    # SBATCH --partition=cpu # -p
+    # SBATCH --cpus-per-task=16 # -c
+    # SBATCH --mem=200gb
+    # SBATCH --output=../%x/%x_%j.log # %x is job-name, %j is job id
+    # SBATCH --account=sci-lippert
+    # SBATCH --time=120:00:00 # -t
 eval "$(conda shell.bash hook)"
 cd ~/projects/MEDS_DEV_NEW || exit
 conda activate meds_dev_311
