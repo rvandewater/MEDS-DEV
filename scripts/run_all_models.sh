c8928f5218815679d2bcfb6639438fab8de3cfbf
Robin van de Water <r.p.vandewater@gmail.com>
2025-11-03 21:56:49 +0100

    run_all_models update

--- scripts/run_all_models.sh
+++ scripts/run_all_models.sh
@@ -7,14 +7,14 @@ if [ "$#" -lt 1 ]; then
 fi
 export MODEL_NAME=$1
 if [[ "$MODEL_NAME" == "genhpf" || "$MODEL_NAME" == "cehrbert" ]]; then
-    # sbatch --job-name="${MODEL_NAME}_HIRID" "${current_path}/run_models_slurm.sh" "$MODEL_NAME" HIRID
-    # sbatch --job-name="${MODEL_NAME}_MIMIC-IV" "${current_path}/run_models_slurm.sh" "$MODEL_NAME" MIMIC-IV
+    sbatch --job-name="${MODEL_NAME}_HIRID" "${current_path}/run_models_slurm.sh" "$MODEL_NAME" HIRID
+    sbatch --job-name="${MODEL_NAME}_MIMIC-IV" "${current_path}/run_models_slurm.sh" "$MODEL_NAME" MIMIC-IV
     sbatch --job-name="${MODEL_NAME}_INSPIRE" "${current_path}/run_models_slurm.sh" "$MODEL_NAME" INSPIRE
     sbatch --job-name="${MODEL_NAME}_NWICU" "${current_path}/run_models_slurm.sh" "$MODEL_NAME" NWICU
-    # sbatch --job-name="${MODEL_NAME}_SICdb" "${current_path}/run_models_slurm.sh" "$MODEL_NAME" SICdb
+    sbatch --job-name="${MODEL_NAME}_SICdb" "${current_path}/run_models_slurm.sh" "$MODEL_NAME" SICdb
     sbatch --job-name="${MODEL_NAME}_AUMCdb" "${current_path}/run_models_slurm.sh" "$MODEL_NAME" AUMCdb
-    # sbatch "${current_path}"/run_models_slurm.sh "$MODEL_NAME" eICU
-    # sbatch "${current_path}"/run_models_slurm.sh "$MODEL_NAME" EHRShot
+    sbatch --job-name="${MODEL_NAME}_eICU" "${current_path}/run_models_slurm.sh" "$MODEL_NAME" eICU
+    sbatch --job-name="${MODEL_NAME}_EHRShot" "${current_path}/run_models_slurm.sh" "$MODEL_NAME" EHRShot
 else
     sbatch --job-name="${MODEL_NAME}_HIRID" "${current_path}/run_models_slurm_cpu.sh" "$MODEL_NAME" HIRID
     sbatch --job-name="${MODEL_NAME}_MIMIC-IV" "${current_path}/run_models_slurm_cpu.sh" "$MODEL_NAME" MIMIC-IV
@@ -22,7 +22,7 @@ else
     sbatch --job-name="${MODEL_NAME}_NWICU" "${current_path}/run_models_slurm_cpu.sh" "$MODEL_NAME" NWICU
     sbatch --job-name="${MODEL_NAME}_SICdb" "${current_path}/run_models_slurm_cpu.sh" "$MODEL_NAME" SICdb
     sbatch --job-name="${MODEL_NAME}_AUMCdb" "${current_path}/run_models_slurm_cpu.sh" "$MODEL_NAME" AUMCdb
-    sbatch "${current_path}"/run_models_slurm_cpu.sh "$MODEL_NAME" eICU
-    sbatch "${current_path}"/run_models_slurm_cpu.sh "$MODEL_NAME" EHRShot
+    sbatch --job-name="${MODEL_NAME}_eICU" "${current_path}/run_models_slurm_cpu.sh" "$MODEL_NAME" eICU
+    sbatch --job-name="${MODEL_NAME}_EHRShot" "${current_path}/run_models_slurm_cpu.sh" "$MODEL_NAME" EHRShot
 fi
 echo "Submitted jobs for model: $MODEL_NAME"
