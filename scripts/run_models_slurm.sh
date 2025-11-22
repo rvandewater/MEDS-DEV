bd3a75325f585bb0b2fc7d55ce08a6d96b5f6d7b
Robin van de Water <r.p.vandewater@gmail.com>
2025-11-22 12:15:57 +0100

    just the right models

--- scripts/run_models_slurm.sh
+++ scripts/run_models_slurm.sh
@@ -6,7 +6,7 @@
 ########## SBATCH --gpus=a40:1
 #SBATCH --output=logs/%x_%j.log # %x is job-name, %j is job id
 #SBATCH --account=sci-lippert
-#SBATCH --time=48:00:00 # -t
+#SBATCH --time=96:00:00 # -t
 #SBATCH -C 'GPU_SKU:A40|GPU_SKU:V100'
 # run with: sbatch run_models_slurm.sh cehrbert HIRID MIMIC-IV
 
