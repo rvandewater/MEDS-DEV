0fe8617e55b5839518fc06817171e2891ac1181b
Robin van de Water <r.p.vandewater@gmail.com>
2025-11-25 18:14:13 +0100

    update with x86

--- scripts/run_models_slurm.sh
+++ scripts/run_models_slurm.sh
@@ -6,9 +6,9 @@
 ########## SBATCH --gpus=a40:1
 #SBATCH --output=logs/%x_%j.log # %x is job-name, %j is job id
 #SBATCH --account=sci-lippert
-#SBATCH --time=96:00:00 # -t
-#SBATCH -C 'GPU_SKU:A40|GPU_SKU:V100'
-
+#SBATCH --time=168:00:00 # -t
+#SBATCH -C 'GPU_SKU:A40|GPU_SKU:V100|GPU_SKU:2080Ti'
+#SBATCH -C 'ARCH:X86'
 # run with: sbatch run_models_slurm.sh cehrbert HIRID MIMIC-IV
 
 # Initialize conda:
