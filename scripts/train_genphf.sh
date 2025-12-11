a72bd0ab7ce0eb9ee22e7f7b67dd5e832552cb22
Robin van de Water <r.p.vandewater@gmail.com>
2025-12-11 18:20:01 +0100

    adjustment

--- scripts/train_genphf.sh
+++ scripts/train_genphf.sh
@@ -6,7 +6,9 @@
 #SBATCH --output=logs/%x_%j.log # %x is job-name, %j is job id
 #SBATCH --account=sci-lippert
 #SBATCH --time=168:00:00 # -t
-#SBATCH -C '(GPU_SKU:A40|GPU_SKU:V100|GPU_SKU:2080Ti)&ARCH:X86'
+##SBATCH -C '(GPU_SKU:A40|GPU_SKU:V100|GPU_SKU:2080Ti)&ARCH:X86'
+#SBATCH --gres=gpu:a40:1
+#SBATCH -C 'ARCH:X86'
 
 # run with: sbatch run_models_slurm.sh cehrbert HIRID MIMIC-IV
 
