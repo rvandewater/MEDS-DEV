6d51976cef2e47f3b11b319efa03b21f829c12dd
Robin van de Water <r.p.vandewater@gmail.com>
2025-12-19 14:59:34 +0100

    update

--- scripts/prep_genphf.sh
+++ scripts/prep_genphf.sh
@@ -107,7 +107,8 @@ for dataset in "${datasets[@]}"; do
         echo "Cleaning up directories before training..."
         if [ -f "$OUTPUT_DIR/data/held_out.tsv" ]; then
             echo "Preprocessed data already exists in $OUTPUT_DIR/data. Skipping cleanup."
-        else
+            continue
+        fi
         rm -rf "$OUTPUT_DIR"
         rm -rf "$FINETUNED_MODEL_DIR"
         rm -rf "$PREDICTIONS_DIR"
