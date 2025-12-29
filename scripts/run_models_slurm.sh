f340a0fcd02f51c9127937fe7792f046259a6f23
Robin van de Water <r.p.vandewater@gmail.com>
2025-12-29 15:53:02 +0100

    more correct

--- scripts/run_models_slurm.sh
+++ scripts/run_models_slurm.sh
@@ -7,7 +7,7 @@
 #SBATCH --output=logs/%x_%j.log # %x is job-name, %j is job id
 #SBATCH --account=sci-lippert
 #SBATCH --time=168:00:00 # -t
-#SBATCH -C #SBATCH -C '(GPU_SKU:A40|GPU_SKU:V100)&ARCH:X86'
+#SBATCH -C '(GPU_SKU:A40|GPU_SKU:V100)&ARCH:X86'
 # run with: sbatch run_models_slurm.sh cehrbert HIRID MIMIC-IV
 
 # Initialize conda:
