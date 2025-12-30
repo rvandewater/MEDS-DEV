085b5a9d8a9eab6f4e572e5d1a59bffad36674eb
Robin van de Water <r.p.vandewater@gmail.com>
2025-12-30 11:59:21 +0100

    skip if predictions exist

--- scripts/run_models_slurm_cpu_new.sh
+++ scripts/run_models_slurm_cpu_new.sh
@@ -170,7 +170,10 @@ for dataset in "${datasets[@]}"; do
             export LABELS_DIR="$DATASET_DIR/labels/$task"
             export FINETUNED_MODEL_DIR="$DATASET_DIR/models/$TASK_NAME/$MODEL_NAME"
             export PREDICTIONS_DIR="$DATASET_DIR/predictions/$TASK_NAME/$MODEL_NAME"
-            export
+            if [ -f "$FINETUNED_MODEL_DIR""/results/**/best_trial/held_out_predictions.parquet" ]; then
+                echo "Skipping: $task because $FINETUNED_MODEL_DIR/results/**/best_trial/held_out_predictions.parquet already exists"
+                continue
+            fi
             meds-dev-model model="$MODEL_NAME" dataset_dir="$DATASET_DIR" labels_dir="$LABELS_DIR" mode=train dataset_type=supervised output_dir="$FINETUNED_MODEL_DIR" model_initialization_dir="$FINETUNED_MODEL_DIR"
             echo "Completed dataset: $DATASET_NAME, task: $task"
         done
